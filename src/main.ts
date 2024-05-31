50266da4308bbca68d9fd2d3714869fa096a00c6
Lumenkowski <lumenkowski@gmail.com>
2024-05-31 18:21:27 +0500

    Example modules

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,5 @@
-console.log( "Package boilerplate" )
+import * as Awesome from "./mod.ts"
+
+Awesome.Logger.log( "Hi!" )
+
+console.log( Awesome.MathUtils.sum( 2, 3 ) )
---
+++ src/mod.ts
@@ -0,0 +1,2 @@
+export * as Logger from "./core/Logger"
+export * as MathUtils from "./math/MathUtils"
